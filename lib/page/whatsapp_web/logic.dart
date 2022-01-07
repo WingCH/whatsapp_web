ad91fe5b2703a0b990b90be2c2103c94113eb01e
WingCH <wingch.apps@gmail.com>
2022-01-07 21:37:01 +0800

    tested in android

--- lib/page/whatsapp_web/logic.dart
+++ lib/page/whatsapp_web/logic.dart
@@ -15,6 +15,8 @@ class WhatsappWebLogic extends GetxController {
     crossPlatform: InAppWebViewOptions(
       useShouldOverrideUrlLoading: true,
       mediaPlaybackRequiresUserGesture: false,
+      userAgent:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Safari/605.1.15',
     ),
     android: AndroidInAppWebViewOptions(
       useHybridComposition: true,
