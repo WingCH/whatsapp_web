cfa197dbb2fa490fed64c38e77bf39d377c4033c
WingCH <wingch.apps@gmail.com>
2022-01-07 21:06:47 +0800

    setup flutter_inappwebview in android

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'page/whatsapp_web/binding.dart';
 import 'page/whatsapp_web/view.dart';
 
-void main() {
+Future<void> main() async {
+  // https://inappwebview.dev/docs/get-started/installation/
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // TODO: iOS Permission
+  await Permission.camera.request();
+  await Permission.microphone.request();
+  await Permission.storage.request();
+
+  if (Platform.isAndroid) {
+    // https://inappwebview.dev/docs/debugging-webviews/
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
+
   runApp(const MyApp());
 }
 
--- lib/page/whatsapp_web/logic.dart
+++ lib/page/whatsapp_web/logic.dart
@@ -1,7 +1,49 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
 
 import 'state.dart';
 
 class WhatsappWebLogic extends GetxController {
   final WhatsappWebState state = WhatsappWebState();
+
+  final GlobalKey webViewKey = GlobalKey();
+  InAppWebViewController? webViewController;
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+    crossPlatform: InAppWebViewOptions(
+      useShouldOverrideUrlLoading: true,
+      mediaPlaybackRequiresUserGesture: false,
+    ),
+    android: AndroidInAppWebViewOptions(
+      useHybridComposition: true,
+    ),
+    ios: IOSInAppWebViewOptions(
+      allowsInlineMediaPlayback: true,
+    ),
+  );
+
+  late PullToRefreshController pullToRefreshController;
+  Rx<String> url = Rx<String>('');
+  Rx<double> progress = Rx<double>(0);
+  final urlController = TextEditingController();
+
+  @override
+  void onInit() {
+    super.onInit();
+    pullToRefreshController = PullToRefreshController(
+      options: PullToRefreshOptions(
+        color: Colors.blue,
+      ),
+      onRefresh: () async {
+        if (Platform.isAndroid) {
+          webViewController?.reload();
+        } else if (Platform.isIOS) {
+          webViewController?.loadUrl(
+              urlRequest: URLRequest(url: await webViewController?.getUrl()));
+        }
+      },
+    );
+  }
 }
